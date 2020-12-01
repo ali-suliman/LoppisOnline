0b9c8ce1d9d9246db8385395a2b46f556a6e26b8
jensengbg-ali-suliman <alx.souliman@gmail.com>
2020-12-01 22:09:14 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="wrapper">
     <mobile-navbar />
+    <Navbar />
+    <animated-image />
   </div>
 </template>
 
 <script>
 import MobileNavbar from "../components/MobileNavbar.vue"
+import AnimatedImage from "../components/AnimatedImage.vue"
+import Navbar from "../components/Navbar.vue"
 export default {
   components: {
     MobileNavbar,
+    AnimatedImage,
+    Navbar,
   },
 }
 </script>
 
 <style lang="scss" scoped>
+@import "../styles/global-styles.scss";
 .wrapper {
   width: 100vw;
+  @include flex();
 }
 </style>
