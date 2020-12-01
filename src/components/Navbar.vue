bc1737c8b3bc9305507893ae4c284f196499830a
jensengbg-ali-suliman <alx.souliman@gmail.com>
2020-12-01 22:09:08 +0100

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav id="nav">
-    <img alt="Vue logo" src="../assets/images/logo.svg" />
+  <nav>
+    <img class="logo" alt="loppisOnline logo" src="../assets/images/logo.svg" />
 
     <section class="links">
       <router-link to="/">hem</router-link>
@@ -21,18 +21,68 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
-#nav {
-  padding: 30px;
+@import "../styles/global-styles.scss";
+
+nav {
+  @include flex();
+  flex-direction: row;
+  justify-content: flex-start;
+  width: 100%;
+  padding: 2rem 6rem;
+
+  .links {
+    margin: 0rem 3rem;
+  }
 
   a {
-    padding: 1rem;
+    padding: 0.8rem 1rem;
     margin: 0rem 1rem;
-    font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-size: 0.85rem;
+    font-weight: bold;
+    font-family: $p-font;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $main-color;
+      border-bottom: 1.9px solid $main-color;
     }
+
+    &:hover {
+      color: $main-color;
+    }
+  }
+
+  .rgstr {
+    margin-left: auto;
+
+    button {
+      border: none;
+      padding: 0.7rem 1.6rem;
+      background: none;
+      font-weight: bold;
+      color: #444;
+      font-size: 0.75rem;
+      cursor: pointer;
+      &:hover {
+        color: $main-color;
+      }
+    }
+
+    .signup {
+      margin: 0rem 1rem;
+      background: $main-color;
+      border-radius: 2rem;
+      color: #f2f2f2;
+      @include cta-animation();
+    }
+  }
+}
+
+@media screen and (max-width: 1200px) {
+  nav {
+    display: none;
   }
 }
 </style>
